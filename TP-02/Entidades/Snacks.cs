9ddb2c1db47de62be999d44cbbea65ee34cfa6ab
AyalaProgramas <31025807+AyalaProgramas@users.noreply.github.com>
2019-05-20 07:34:43 -0300

    Add files via upload

---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks:Producto
+    {
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            :base(marca,patente,color)
+        {
+
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+        /// <summary>
+        /// Muestra los datos de Snack
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : "+ this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
